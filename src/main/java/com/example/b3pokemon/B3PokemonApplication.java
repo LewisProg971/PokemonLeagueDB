f5e989ef3e1f366ef8ed25e1eb6044edba2ae82e
Lewis CRANE <103712344+LewisProg971@users.noreply.github.com>
2026-03-04 15:33:35 +0100

    initial commit

---
+++ src/main/java/com/example/b3pokemon/B3PokemonApplication.java
@@ -0,0 +1,13 @@
+package com.example.b3pokemon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class B3PokemonApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(B3PokemonApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/b3pokemon/B3PokemonApplicationTests.java
@@ -0,0 +1,7 @@
+package com.example.b3pokemon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class B3PokemonApplicationTests {}
